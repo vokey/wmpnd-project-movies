e0275cefb3928132fc1ac34b46aa2a876057d281
Jinpeng LI <code@lijinpeng.cn>
2019-02-10 04:22:34 +0800

    Add bindtap and oncall functions

--- miniprogram/pages/movies/details/details.js
+++ miniprogram/pages/movies/details/details.js
@@ -5,7 +5,9 @@ Page({
    * Page initial data
    */
   data: {
-
+    cover: "https://covers-1254341575.cos.ap-shanghai.myqcloud.com/p2496940327.jpg",
+    title: "海边的曼彻斯特",
+    description: "李（卡西·阿弗莱克 Casey Affleck 饰）是一名颓废压抑的修理工，在得知哥哥乔伊（凯尔·钱德勒 Kyle Chandler 饰）去世的消息后，李回到了故乡——海边的曼彻斯特处理乔伊的后事。根据乔伊的遗嘱，李将会成为乔伊的儿子帕特里克（卢卡斯·赫奇斯 Lucas Hedg es 饰）的监护人，李打算将帕特里克带回波士顿，但很显然帕特里克并不愿意离开家乡和朋友们，但李亦不愿在这片伤心地久留。原来，曼彻斯特埋藏着李的一段绝望的回忆，他的过失使得两个女儿葬身火海，妻子兰迪（米歇尔·威廉姆斯 Michelle Williams 饰）亦因此而离开了他。此次重回故乡，李再度见到了已经再婚并且即将做妈妈的兰迪，与此同时，帕特里克那失踪已久的母亲艾丽斯（格瑞辰·摩尔 Gretchen Mol 饰）亦联系上了帕特里克，希望他能够成为她的新家庭中的一员。",
   },
 
   /**
@@ -62,5 +64,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onTapView() {
+    wx.navigateTo({
+      url: '/pages/comments/list/list',
+    })
+  },
+
+  onTapComment() {
+    wx.showActionSheet({
+      itemList: ["文字", "音频"],
+      success: res => {
+        console.log(res.tapIndex)
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
   }
 })
